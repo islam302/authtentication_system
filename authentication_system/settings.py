d7c3112615990516312390813457f651bdc8e24c
Islam Badran <islambadran39@gmail.com>
2026-03-12 01:19:02 +0200

    update time of access token

--- authentication_system/settings.py
+++ authentication_system/settings.py
@@ -130,7 +130,7 @@ REST_FRAMEWORK = {
 # ─── SimpleJWT ────────────────────────────────────────────────────────────────
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=3),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
